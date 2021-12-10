16d3ecf3e44be10fd2dfe754a8e131530a8997c5
Na1s <imna1se@gmail.com>
2021-12-10 18:31:28 +0500

    add class Main_page, methods go_to_login_page add

---
+++ Page/main_page.py
@@ -0,0 +1,8 @@
+from base_page import Base_Page
+from selenium.webdriver.common.by import By
+
+
+class Main_Page(Base_Page):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
