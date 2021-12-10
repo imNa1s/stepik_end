eb462590bd250d3c8f25ac3bc58b4b1bb6ce8622
Na1s <imna1se@gmail.com>
2021-12-10 19:55:30 +0500

    add check login link available

--- Page/main_page.py
+++ Page/main_page.py
@@ -6,3 +6,6 @@ class Main_Page(Base_Page):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
