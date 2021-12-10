2449dbf327c2059054c25529f455481dd85eba25
Na1s <imna1se@gmail.com>
2021-12-10 18:59:51 +0500

    rebuild test with PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,8 @@
-import time
-from selenium.webdriver.common.by import By
-
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .Page.main_page import Main_Page
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    Page = Main_Page(browser, link)
+    Page.open()
+    Page.go_to_login_page()
