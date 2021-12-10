a9e79f6a28b0e591ea086abea511a62b50943d23
Na1s <imna1se@gmail.com>
2021-12-10 16:24:39 +0500

    split function for better looking

--- test_main_page.py
+++ test_main_page.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_diff_language(browser):
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
     browser.get(link)
-    browser.implicitly_wait(10)
-    check = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
-    assert len(check) > 0, "can't find button"
-    time.sleep(5)
+    go_to_login_page(browser)
