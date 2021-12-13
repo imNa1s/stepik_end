5ad696143e899e7ad100837bb23df7d46dc61051
Na1s <imna1se@gmail.com>
2021-12-13 17:39:29 +0500

    make check name and price correct

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,14 @@
 from .Page.product_page import Product_NY
-
+import time
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     Page = Product_NY(browser, link)
     Page.open()
     Page.try_find_add()
     Page.add_to_basket()
     Page.solve_quiz_and_get_code()
+    Page.check_basket_equal_by_name()
+    Page.check_basket_equal_by_prise()
+
 
