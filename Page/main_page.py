52300c052ff05ac3c28a3ab576e962bbf67b7b0c
Na1s <imna1se@gmail.com>
2021-12-13 12:24:05 +0500

    change link and CSS selector

--- Page/main_page.py
+++ Page/main_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 class Main_Page(Base_Page):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#registration_link")
         login_link.click()
 
     def should_be_login_link(self):
--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,7 @@ from .Page.main_page import Main_Page
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     Page = Main_Page(browser, link)
     Page.open()
     Page.go_to_login_page()
